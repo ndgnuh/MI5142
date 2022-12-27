45303adfa30441f2e16a8858950389a7a565a58c
Hùng Nguyễn <ndgnuh@protonmail.com>
2022-12-27 23:56:13 +0700

    5 & 10

--- final-codes/cau-05.sql
+++ final-codes/cau-05.sql
@@ -1,45 +1,100 @@
-create or alter trigger maximum_student_registered on takes
-after insert 
-as begin
-    declare @cap int;
-    declare @building int;
-    declare @sec_id int;
-    declare @course_id int;
-    declare @semester int;
-    declare @year int;
-    declare @cnt int;
+GO
+CREATE OR ALTER TRIGGER TRIG_MAX_ROOM_CAPACITY ON TAKES
+AFTER INSERT 
+AS BEGIN
+    DECLARE @CAP INT;
+    DECLARE @BUILDING VARCHAR(15);
+    DECLARE @SEC_ID INT;
+    DECLARE @COURSE_ID INT;
+    DECLARE @SEMESTER VARCHAR(8);
+    DECLARE @YEAR INT;
+    DECLARE @CNT INT;
     
-    -- get info
-    select
-        @sec_id = sec_id,
-        @course_id = course_id,
-        @semester = semester,
-        @year = year
-    from inserted
+    -- GET INFO
+    SELECT
+        @SEC_ID = SEC_ID,
+        @COURSE_ID = COURSE_ID,
+        @SEMESTER = SEMESTER,
+        @YEAR = YEAR
+    FROM INSERTED
     
-    -- get building
-    select top 1 @building = building from section 
-    where sec_id = @sec_id 
-        and course_id = @course_id
-        and semester = @semester
-        and year = @year
+    -- GET BUILDING
+    SELECT TOP 1 @BUILDING = BUILDING FROM SECTION 
+    WHERE SEC_ID = @SEC_ID 
+        AND COURSE_ID = @COURSE_ID
+        AND SEMESTER = @SEMESTER
+        AND YEAR = @YEAR
     
         
-    --  get current capacity
-    select @cnt = count(id) from takes
-    where sec_id = @sec_id 
-        and course_id = @course_id
-        and semester = @semester
-        and year = @year
+    --  GET CURRENT CAPACITY
+    SELECT @CNT = COUNT(ID) FROM TAKES
+    WHERE SEC_ID = @SEC_ID 
+        AND COURSE_ID = @COURSE_ID
+        AND SEMESTER = @SEMESTER
+        AND YEAR = @YEAR
     
-    -- find room max capacity
-    select @cap = capacity from classroom where building = @building
+    -- FIND ROOM MAX CAPACITY
+    SELECT @CAP = CAPACITY FROM CLASSROOM WHERE BUILDING = @BUILDING
     
-    -- check caps
-    if (@cnt > @cap)
-    begin
-        raiserror('Phòng quá sức chứa', 1, 1)
-        rollback transaction;  
-    end
-    return ;
-end
+    -- CHECK CAPS
+    IF (@CNT > @CAP)
+    BEGIN
+        RAISERROR('PHÒNG QUÁ SỨC CHỨA', 1, 1)
+        ROLLBACK TRANSACTION;  
+    END
+    RETURN ;
+END
+
+GO
+CREATE OR ALTER VIEW VW_CAPACITY AS
+SELECT 
+    SECTION.SEC_ID,
+    SECTION.BUILDING, 
+    SECTION.ROOM_NUMBER, 
+    SECTION.YEAR, 
+    SECTION.SEMESTER, 
+    TIME_SLOT_ID,
+    CLASSROOM.CAPACITY,
+    SECTION.COURSE_ID,
+    COUNT(DISTINCT TAKES.ID) AS TOTAL
+FROM 
+    CLASSROOM INNER JOIN SECTION
+        ON CLASSROOM.BUILDING = SECTION.BUILDING 
+        AND SECTION.ROOM_NUMBER = CLASSROOM.ROOM_NUMBER
+    INNER JOIN TAKES
+        ON TAKES.SEC_ID = SECTION.SEC_ID
+        AND TAKES.COURSE_ID = SECTION.COURSE_ID
+    GROUP BY 
+        TAKES.COURSE_ID,
+        SECTION.SEC_ID,
+        SECTION.BUILDING, 
+        SECTION.ROOM_NUMBER, 
+        SECTION.YEAR, 
+        SECTION.SEMESTER, 
+        SECTION.TIME_SLOT_ID,
+        SECTION.COURSE_ID,
+        CLASSROOM.CAPACITY
+
+GO
+SELECT * FROM VW_CAPACITY ORDER BY TOTAL 
+
+GO
+UPDATE CLASSROOM SET CAPACITY=270 WHERE BUILDING = 'CHANDLER' AND ROOM_NUMBER = 804
+GO
+SELECT * FROM VW_CAPACITY WHERE BUILDING = 'CHANDLER' AND ROOM_NUMBER = 804  ORDER BY TOTAL
+
+GO
+DELETE FROM TAKES WHERE ID=24746 AND COURSE_ID='313'
+GO
+-- ERROR
+-- EXEC SP_REGISTER '24746', '313', '804', 'CHANDLER', 'N', '2010', 'FALL'
+GO
+UPDATE CLASSROOM SET CAPACITY=270 WHERE BUILDING = 'CHANDLER' AND ROOM_NUMBER = 804
+GO
+SELECT * FROM VW_CAPACITY WHERE BUILDING = 'CHANDLER' AND ROOM_NUMBER = 804  ORDER BY TOTAL
+
+GO
+DELETE FROM TAKES WHERE ID=24746 AND COURSE_ID='313'
+GO
+EXEC SP_REGISTER '24746', '313', '804', 'CHANDLER', 'N', '2010', 'FALL'
+DELETE FROM TAKES WHERE ID=24746 AND COURSE_ID='313'
--- final-codes/cau-10.sql
+++ final-codes/cau-10.sql
@@ -1,131 +1,63 @@
 go
-create or alter view view_register as
-select
-    takes.id,
-    takes.course_id,
-    section.room_number,
-    section.building,
-    section.time_slot_id,
-    takes.year,
-    takes.semester,
-    takes.sec_id
-from takes inner join section on takes.course_id = section.course_id 
-    and takes.sec_id = section.sec_id
-    and takes.semester = section.semester
-    and takes.year = section.year
-    
-go
-select * from view_register
-
-go
 create or alter procedure sp_register
   @id varchar(5),
   @course_id varchar(8),
-  @building varchar(15),
   @room_number varchar(7),
+  @building varchar(15),
   @time_slot_id varchar(4),
   @year numeric,
   @semester varchar(6)
 as
 begin
-    declare @sec_id varchar(8)
-    (select @sec_id = sec_id from section where 
-        building = @building and
-        room_number = @room_number and
-        time_slot_id = @time_slot_id and
-        year = @year and
-        semester = @semester)
-    
     begin transaction txn
     
+    if not exists(select sec_id from section where room_number = @room_number and course_id = @course_id and building = @building and time_slot_id = @time_slot_id and year = @year and semester = @semester) begin
+        raiserror('no such class', 15, 1)
+        rollback tran txn
+        return
+    end
+    
+    -- FIND @sec_id
+    declare @sec_id varchar(8);
+    select @sec_id = sec_id from section where room_number = @room_number and course_id = @course_id and building = @building and time_slot_id = @time_slot_id and year = @year and semester = @semester
+
+    -- CHECK IF ALREADY REGISTERED
+    if exists(select id from takes where id = @id and course_id = @course_id and year = @year and semester = @semester) begin
+        raiserror('already registered', 15, 1)
+        rollback tran txn
+        return
+    end
         
+    -- REGISTER
+    insert into takes
+        (id, course_id, year, semester, sec_id) 
+    values
+        (@id, @course_id, @year, @semester, @sec_id)
+        
+    -- NO SUCH STUDENT
     if not exists(select id from student where id = @id)
     begin
         raiserror('bad student id', 15, 1)
         rollback tran txn
+        return
     end
+    
+    -- NO SUCH COURSE
     else if not exists(select course_id from course where course_id = @course_id)
     begin
         raiserror('bad course id', 15, 1)
         rollback tran txn
+        return
     end
-    else if not exists(select sec_id from section where 
-        building = @building and
-        room_number = @room_number and
-        time_slot_id = @time_slot_id and
-        year = @year and
-        semester = @semester)
-    begin
-        raiserror('bad class time', 15, 1)
-        rollback tran txn
-    end
-    else begin
-        insert into takes
-            (id, course_id, year, semester, sec_id) 
-        values
-            (@id, @course_id, @year, @semester, @sec_id)
-        commit
-    end
+    
+    -- OK
+    commit tran txn
 end
 
-GO
-create or alter trigger maximum_student_registered on takes
-after insert 
-as begin
-    declare @cap int;
-    declare @building varchar(15);
-    declare @sec_id int;
-    declare @course_id @course_id varchar(8);
-    declare @semester @semester varchar(6);
-    declare @year int;
-    declare @cnt int;
-    declare @room_number varchar(7);
-    declare @time_slot_id varchar(4);
-  
-    
-    -- get info
-    -- select
-    --     @sec_id = sec_id,
-    --     @course_id = course_id,
-    --     @semester = semester,
-    --     @year = year
-    -- from inserted
-    
-    -- -- get building
-    -- select top 1 @building = building from section 
-    -- where sec_id = @sec_id 
-    --     and course_id = @course_id
-    --     and semester = @semester
-    --     and year = @year
-    
-        
-    -- --  get current capacity
-    -- select @cnt = count(id) from takes
-    -- where sec_id = @sec_id 
-    --     and course_id = @course_id
-    --     and semester = @semester
-    --     and year = @year
-    
-    -- -- find room max capacity
-    -- select @cap = capacity from classroom where building = @building
-    
-    -- -- check caps
-    -- if (@cnt > @cap)
-    -- begin
-    --     raiserror('Phòng quá sức chứa', 1, 1)
-    --     rollback transaction;  
-    -- end
-    -- return ;
-end
 
+exec sp_register '41973', '200', '180', 'Saucon', 'D', '2007', 'Spring'
+DELETE FROM takes WHERE ID=41973 AND course_id=200 
 
 go
--- drop trigger maximum_student_registered
---   @id varchar(5),
---   @course_id varchar(8),
---   @building varchar(15),
---   @room_number varchar(7),
---   @time_slot_id varchar(4),
---   @year numeric,
---   @semester varchar(6)
-exec sp_register '10012320', '200', 'Saucon', '180', 'D', '2007', 'Spring'
+select * from section where course_id = 313 
+
